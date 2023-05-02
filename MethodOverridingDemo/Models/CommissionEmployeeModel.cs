f38c4f9f14817cbc537fe32b4b317cf03a8e2e92
BeardedBiologist <46723997+BeardedBiologist@users.noreply.github.com>
2023-05-02 12:49:29 +0200

    init commit

---
+++ MethodOverridingDemo/Models/CommissionEmployeeModel.cs
@@ -0,0 +1,13 @@
+﻿namespace MethodOverridingDemo
+{
+    public class CommissionEmployeeModel : EmployeeModel
+    {
+        public decimal CommisionAmount { get; set; }
+
+        public override decimal GetPayCheckAmount(int hoursWorked)
+        {
+            decimal initialPay = base.GetPayCheckAmount(hoursWorked);
+            return initialPay + CommisionAmount;
+        }
+    }
+}
--- MethodOverridingDemo/Program.cs
+++ MethodOverridingDemo/Program.cs
@@ -1,3 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
 
+namespace MethodOverridingDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PersonModel person = new PersonModel
+            {
+                FirstName = "Tim",
+                LastName = "Corey",
+                Email = "contact@timecorey.com"
+            };
+            Console.WriteLine(person);
+
+        }
+    }
+    public abstract class Car
+    {
+        public virtual void StartCar()
+        {
+            Console.WriteLine("Turn key and start");
+        }
+    }
+}
